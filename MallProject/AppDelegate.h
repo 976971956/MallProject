06ea1dd7bce6ef817ae6282842d49552ac718e53
UJF <976971956@qq.com>
2019-06-03 16:24:47 +0800

    fister commit

---
+++ MallProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MallProject
+//
+//  Created by 江湖 on 2019/6/3.
+//  Copyright © 2019 江湖. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MallProjectTests/MallProjectTests.m
@@ -0,0 +1,37 @@
+//
+//  MallProjectTests.m
+//  MallProjectTests
+//
+//  Created by 江湖 on 2019/6/3.
+//  Copyright © 2019 江湖. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MallProjectTests : XCTestCase
+
+@end
+
+@implementation MallProjectTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
